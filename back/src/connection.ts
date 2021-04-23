acdd512fa786a7f1aa1a3414cffba5bc4e2e29b6
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2021-04-23 11:08:08 +0300

    fixed connection to mongo

--- back/src/connection.ts
+++ back/src/connection.ts
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 require('dotenv').config();
-const MONGODB_URI = process.env.MONGODB_URI;
 export const connectDb = () => {
-  console.log('connecting to', MONGODB_URI);
-  return mongoose.connect(MONGODB_URI, {
+  console.log('connecting to', 'mongodb://mongo:27017');
+  return mongoose.connect('mongodb://mongo:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
