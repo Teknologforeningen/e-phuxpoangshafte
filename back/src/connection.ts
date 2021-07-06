dc5379b71544454443067234519ed5362d1d6cf4
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2021-07-06 19:50:14 +0300

    fix db

--- back/src/connection.ts
+++ back/src/connection.ts
@@ -1,20 +1,24 @@
 import { Client } from 'pg';
 
 const client = new Client({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  user: process.env.POSTGRES_USER || 'ephuxpoang',
+  password: process.env.POSTGRES_PASSWORD || 'ephuxpoang',
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DB
-})
+  database: process.env.POSTGRES_DB || 'phuxdb',
+});
 
 export const connectionTest = () => {
-  client.connect()
-  .then(()=> {
-    console.log("Connected succesfuly to the database")})
-  .catch((e) =>{
-    console.error(e)})
-  .finally(() => {
-    client.end()
-  })
-}
+  client
+    .connect()
+    .then(() => {
+      console.log(client);
+      console.log('Connected succesfuly to the database');
+    })
+    .catch(e => {
+      console.error(e);
+    })
+    .finally(() => {
+      client.end();
+    });
+};
